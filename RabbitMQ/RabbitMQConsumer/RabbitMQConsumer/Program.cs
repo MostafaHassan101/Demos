918f4fd3bc5c95b778acafd4b8033f7543ccdb49
MostafaHassan101 <mostafa157716@gmail.com>
2024-11-10 01:31:57 +0200

    first commit

---
+++ RabbitMQ/RabbitMQConsumer/RabbitMQConsumer/Program.cs
@@ -0,0 +1,40 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+try
+{
+
+	var factory = new ConnectionFactory() { HostName = "localhost" }; // Use your RabbitMQ server hostname
+	using (var connection = await factory.CreateConnectionAsync())
+	using (var channel = await connection.CreateChannelAsync())
+	{
+		await channel.QueueDeclareAsync(queue: "demoQueue",
+							 durable: false,
+							 exclusive: false,
+							 autoDelete: false,
+							 arguments: null);
+
+		var consumer = new AsyncEventingBasicConsumer(channel);
+		consumer.ReceivedAsync += async (model, ea) =>
+		{
+			var body = ea.Body.ToArray();
+			var message = Encoding.UTF8.GetString(body);
+			Console.WriteLine($" [x] Received {message}");
+
+			// Simulate an asynchronous operation
+			await Task.Yield(); // Ensures the method is asynchronous
+		};
+		await channel.BasicConsumeAsync(queue: "demoQueue",
+							 autoAck: true,
+							 consumer: consumer);
+
+		Console.WriteLine(" Press [enter] to exit.");
+		Console.ReadLine();
+	}
+}
+catch (Exception ex)
+{
+	Console.WriteLine(ex.Message);
+}
---
+++ RabbitMQ/RabbitMQProducer/RabbitMQProducer/Program.cs
@@ -0,0 +1,30 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+try
+{
+    var factory = new ConnectionFactory() { HostName = "localhost" }; // Use your RabbitMQ server hostname
+    using (var connection = await factory.CreateConnectionAsync())
+    using (var channel = await connection.CreateChannelAsync())
+    {
+        await channel.QueueDeclareAsync(queue: "demoQueue",
+                             durable: false,
+                             exclusive: false,
+                             autoDelete: false,
+                             arguments: null);
+
+        string message = "Hello, RabbitMQ - 4!!!";
+        var body = Encoding.UTF8.GetBytes(message);
+
+        await channel.BasicPublishAsync(exchange: "", routingKey: "demoQueue", body: body, cancellationToken: CancellationToken.None);
+        Console.WriteLine($" [x] Sent {message}");
+    }
+
+    Console.WriteLine(" Press [enter] to exit.");
+	Console.ReadKey();
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.ToString());
+}
